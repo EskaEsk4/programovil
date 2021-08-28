7f447ff4ff6c060c7539a7f85b42f31f2cde262d
Eskarlette <eskarlette.celeste@gmail.com>
2021-08-27 21:51:55 -0400

    confirmar

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,18 +1,37 @@
 import { Component } from '@angular/core';
-
+//Importación de control de formularios
+import { FormControl, FormGroup } from '@angular/forms';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
+  //en su interior se declaran los objetos a inyectar en la pagina TypeScript
   constructor() {}
+  //defición de variables nombre; tipo ="valor inicial"
+  // String, boolean, number, any(cualquier dato), array(definición de arreglo)
   rut:String = "";
   nombreCompleto:String = "";
   edad:BigInteger;
-  cambiarDatos(){
-    alert('pinchó botón');
-  }
 
+
+// declaracion de objetos tipo formulario
+elrut = new FormControl('');
+elNombreCompleto = new FormControl('');
+elEdad = new FormControl('');
+
+
+// Grupo de controles de persona
+persona = new FormGroup({
+  elrut : new FormControl(''),
+  elNombreCompleto : new FormControl (''),
+  laEdad : new FormControl ('')
+});
+
+  //Métodos de acceso
+  cambiarDatos(){
+    alert('pinchó botón'+this.rut);
+  }
+//IMPLEMENTACIÓN DE CÓDIGO EN JAVASCRIPT
 }
